d1b0a3e7633a51333c13e2cd218212a67f33e6d1
Sarath Pantula <sarath.pantula@gmail.com>
2023-01-14 19:30:27 +0530

    Configure Core Extensions

---
+++ core/Extensions/StartUpExtensions/CoreExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace core.Extensions.StartUpExtensions;
+
+/// <summary>
+/// Core Extension
+/// </summary>
+public static class CoreExtension
+{
+    /// <summary>
+    /// Configure Core Extensions
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection ConfigureCoreExtensions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.ConfigureIOptions(configuration);
+        services.ConfigureBaseExtensions();
+        services.ConfigureNewtonsoftJson();
+        services.ConfigureSwagger(configuration);
+
+        return services;
+    }
+}
