04c1dc7332141b63a975f15998f0330bd4853161
Sarath Pantula <sarath.pantula@gmail.com>
2023-03-21 17:24:56 +0530

    Add File Utility and Json Utility

---
+++ core/Utilities/FileUtility.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace core.Utilities;
+
+/// <summary>
+/// File Utility
+/// </summary>
+public static class FileUtility
+{
+    /// <summary>
+    /// Read File Contents Async
+    /// </summary>
+    /// <param name="file">Implements <see cref="IFormFile"/></param>
+    /// <returns>Returns <see cref="string"/></returns>
+    public async static Task<string> ReadFileContentsAsync(IFormFile file)
+    {
+        using var reader = new StreamReader(file.OpenReadStream());
+        return await reader.ReadToEndAsync();
+    }
+}
---
+++ core/Utilities/JsonUtility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace core.Utilities;
+
+/// <summary>
+/// Json Utility
+/// </summary>
+public static class JsonUtility
+{
+    /// <summary>
+    /// Validate Json
+    /// </summary>
+    /// <param name="file">Implements <see cref="IFormFile"/></param>
+    /// <returns>Returns <see cref="bool"/></returns>
+    public async static Task<bool> ValidateJson(IFormFile file)
+    {
+        string jsonString = await FileUtility.ReadFileContentsAsync(file);
+        try
+        {
+            using JsonDocument document = JsonDocument.Parse(jsonString);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
