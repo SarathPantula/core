a2c1e5792eac6fa138c02cbc5ac8c213e98e7077
Sarath Pantula <sarath.pantula@gmail.com>
2023-06-29 21:45:58 +0530

    Add Guid Generator

---
+++ core/Base/GuidGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace core.Base;
+
+/// <summary>
+/// Guid Generator
+/// </summary>
+public static class GuidGenerator
+{
+    /// <summary>
+    /// Generate Random Guid
+    /// </summary>
+    /// <returns>Returns a randomly generated guid</returns>
+    public static Guid Generate()
+    {
+        byte[] bytes = new byte[16];
+
+        // Generate random byte values for the complex GUID
+        Random random = new Random();
+        random.NextBytes(bytes);
+
+        // Modify specific bytes with random values
+        bytes[0] = (byte)(bytes[0] ^ random.Next(256));
+        bytes[1] = (byte)(bytes[1] ^ random.Next(256));
+        bytes[2] = (byte)(bytes[2] ^ random.Next(256));
+        bytes[3] = (byte)(bytes[3] ^ random.Next(256));
+
+        Guid complexGuid = new Guid(bytes);
+        return complexGuid;
+    }
+}
---
+++ core/Models/Entity.cs
@@ -0,0 +1,45 @@
+﻿namespace core.Models;
+
+/// <summary>
+/// Entity
+/// </summary>
+public class Entity
+{
+	/// <summary>
+	/// ctor
+	/// </summary>
+	/// <param name="id">Entity Id</param>
+	/// <param name="name">Entity Name</param>
+	/// <param name="displayName">Display Name</param>
+	/// <param name="isActive">Is Active</param>
+    public Entity(Guid id,
+		string name,
+		string displayName,
+		bool isActive)
+	{
+		Id = id;
+		Name = name;
+		DisplayName = displayName;
+		IsActive = isActive;
+	}
+
+	/// <summary>
+	/// Id
+	/// </summary>
+	public Guid Id { get; set; }
+
+	/// <summary>
+	/// Name
+	/// </summary>
+	public string Name { get; set; } = null!;
+
+	/// <summary>
+	/// Display Name
+	/// </summary>
+	public string DisplayName { get; set; } = null!;
+
+	/// <summary>
+	/// IsActive
+	/// </summary>
+	public bool IsActive { get; set; } = true;
+}
